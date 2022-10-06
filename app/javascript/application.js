dc14eaf474c25e3c0de1e8e6e5af7d8195098968
Divagueame <martin.arce.teixeira@gmail.com>
2022-10-06 12:22:50 +0200

    Add jobs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,9 +4,12 @@ import consumer from './channels/consumer'
 import CableReady from "cable_ready"
 import mrujs from "mrujs";
 import { CableCar } from "mrujs/plugins"
+import "trix"
+import "@rails/actiontext"
 
 mrujs.start({
   plugins: [
     new CableCar(CableReady)
   ]
 })
+
