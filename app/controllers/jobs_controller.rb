af37eefc5c7771b0f8d1639c1d7acaf0ec019c78
Divagueame <martin.arce.teixeira@gmail.com>
2022-10-06 15:49:46 +0200

    new Job Slideover

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -13,7 +13,12 @@ class JobsController < ApplicationController
 
   # GET /jobs/new
   def new
-    @job = Job.new
+    html = render_to_string(partial: 'form', locals: { job: Job.new })
+    p html
+    p cable_car
+    render operations: cable_car
+      .inner_html('#slideover-content', html: html)
+      .text_content('#slideover-header', text: 'Post a new job')
   end
 
   # GET /jobs/1/edit
